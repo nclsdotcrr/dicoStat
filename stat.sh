1aa5817ed78335c99d12a4a635f73dfde6a140ee
nclsdotcrr <ncls.crr@gmail.com>
2014-12-27 20:36:41 +0100

    saisie du nom du fichier dictionnaire

--- stat.sh
+++ stat.sh
@@ -1,10 +1,11 @@
+read -p 'nom du fichier a analyser : ' fichier
+
 # Declaration de mes constantes
-fichier="dico.txt"
+# fichier="dico.txt"
 
 # Declaration de mes variables
 var="z"
 
 # Compteur d'occurrence de "var"
 echo -e $var | grep -oi "$var" $fichier | wc -l
-
 echo 'nombre de lettres :' | wc -c dico.txt
