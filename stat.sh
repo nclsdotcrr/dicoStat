01f8618f741e756ff208ab9e1046b62f1413162d
nclsdotcrr <ncls.crr@gmail.com>
2014-12-27 20:57:38 +0100

    compteur de caractères "z"

--- stat.sh
+++ stat.sh
@@ -7,5 +7,5 @@ read -p 'nom du fichier a analyser : ' fichier
 var="z"
 
 # Compteur d'occurrence de "var"
-echo -e $var | grep -oi "$var" $fichier | wc -l
-echo 'nombre de lettres :' | wc -c test.txt
+echo -e $var | grep -oi "$var" $fichier | wc -m
+echo 'nombre de lettres :' | wc -m test.txt
