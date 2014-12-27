0dd8935a2c53402804541661205f64fdfecb37f8
nclsdotcrr <ncls.crr@gmail.com>
2014-12-27 20:21:31 +0100

    Création du bash star.sh

---
+++ stat.sh
@@ -0,0 +1,10 @@
+# Declaration de mes constantes
+fichier="dico.txt"
+
+# Declaration de mes variables
+var="z"
+
+# Compteur d'occurrence de "var"
+echo -e $var | grep -oi "$var" $fichier | wc -l
+
+echo 'nombre de lettres :' | wc -c dico.txt
