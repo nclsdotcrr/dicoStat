6254ca387d3697581170eb989f5385dd86639488
ncls.crr <ncls.crr@gmail.com>
2014-12-27 21:57:20 +0100

    nom du fichier caché dans le comptage des lettres

--- stat.sh
+++ stat.sh
@@ -1,5 +1,5 @@
 clear
-# Lecture du fichier dico
+# Lecture du fichier a analyser
 # read -p 'nom du fichier a analyser : ' fichier
 fichier='dico.txt'
 var="z"
@@ -7,5 +7,5 @@ var="z"
 # Compteur d'occurrences de la lettre var
 echo 'Occurrence de la lettre' $var :
 echo -e $var | grep -oi "$var" $fichier | wc -l
-echo 'nombre de lettres :'
-wc -m dico.txt
+echo 'Nombre total de lettres dans le fichier :'
+wc -m <dico.txt
