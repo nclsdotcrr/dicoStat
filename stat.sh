c128c2bc868082cc662a71940a378930b840c4f3
ncls.crr <ncls.crr@gmail.com>
2014-12-27 21:40:58 +0100

    nettoyage

--- stat.sh
+++ stat.sh
@@ -1,10 +1,11 @@
 clear
-#read -p 'nom du fichier a analyser : ' fichier
+# Lecture du fichier dico
+# read -p 'nom du fichier a analyser : ' fichier
 fichier='dico.txt'
 var="z"
 
-# Compteur d'occurrence de "var"
+# Compteur d'occurrences de la lettre var
+echo 'Occurrence de la lettre' $var :
 echo -e $var | grep -oi "$var" $fichier | wc -l
-echo 'nombre de lettres :' | wc -c test.txt
-echo -e $var | grep -oi "$var" $fichier | wc -m
-echo 'nombre de lettres :' | wc -m dico.txt
+echo 'nombre de lettres :'
+wc -m dico.txt
