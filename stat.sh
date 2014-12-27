fc12a625b789c6a1b60c3ad7a01bd345cd878e97
ncls.crr <ncls.crr@gmail.com>
2014-12-27 21:33:20 +0100

    fonctionne pas trop mal

--- stat.sh
+++ stat.sh
@@ -1,7 +1,10 @@
-# Declaration de mes variables
-read -p 'nom du fichier a analyser : ' fichier
-var='z'
+clear
+#read -p 'nom du fichier a analyser : ' fichier
+fichier='dico.txt'
+var="z"
 
 # Compteur d'occurrence de "var"
+echo -e $var | grep -oi "$var" $fichier | wc -l
+echo 'nombre de lettres :' | wc -c test.txt
 echo -e $var | grep -oi "$var" $fichier | wc -m
 echo 'nombre de lettres :' | wc -m dico.txt
